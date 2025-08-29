d88e789c987907991dab474d8b16f7a87f93ffb5
SkandhanM <55359879+SKANDHAN-MADHUSUDHANA@users.noreply.github.com>
2025-08-29 00:43:24 -0400

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,1 +1,14 @@
-console.log("This is a popup!");
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelectorAll(".link-box").forEach(item => {
+    item.addEventListener("click", () => {
+      const link = item.getAttribute("data-link");
+      navigator.clipboard.writeText(link).then(() => {
+        // Feedback
+        item.textContent = item.textContent + " ✅";
+        setTimeout(() => {
+          item.textContent = item.textContent.replace(" ✅", "");
+        }, 1000);
+      });
+    });
+  });
+});
